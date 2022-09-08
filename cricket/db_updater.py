36e9902cb37222e793d1935a6ced39734f9d9eba
realhardik18 <realhardik18@gmail.com>
2022-09-08 17:08:40 +0530

    finshed with online db hookup

---
+++ cricket/db_updater.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from creds import PANTRY_ID
+
+url = f"https://getpantry.cloud/apiv1/pantry/{PANTRY_ID}/basket/cricket data"
+
+
+def get_pantry():
+    payload = ""
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.json()
+
+
+def update_pantry(item):
+    data = get_pantry()
+    data['webhooks'].append(item)
+    payload = json.dumps(data)
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    requests.request("POST", url, headers=headers, data=payload)
+
+
+# print(update_pantry('yo00'))
+# print(get_pantry())
--- cricket/main.py
+++ cricket/main.py
@@ -5,17 +5,16 @@ from dataGetter import get_commentary, get_overs, get_toss_result, get_score, ge
 from classes import class_commentary, class_for_toss, class_overs, class_score
 from matchinfo import location, title, match_url
 from webhooksender import send_webhook
-with open('webhooks.json', 'r') as f:
-    webhook_urls = json.load(f)['webhooks']
+from db_updater import get_pantry
 
-send_webhook(webhooks=webhook_urls)
+send_webhook(webhooks=get_pantry()['webhooks'])
 overs = get_overs(url=match_url, class_for_overs=class_overs)
 
 while True:
     if overs != get_overs(url=match_url, class_for_overs=class_overs):
         overs = get_overs(url=match_url, class_for_overs=class_overs)
         #sent_webhook = webhook.edit(sent_webhook)
-        send_webhook(webhook_urls)
+        send_webhook(webhooks=get_pantry()['webhooks'])
         print('sent webhook')
     else:
         print(overs)
