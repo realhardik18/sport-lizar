05f505192210491cc2d935a9f32bdef5529e6ab2
realhardik18 <realhardik18@gmail.com>
2022-09-08 17:20:54 +0530

    delted some variable files

--- cricket/classes.py
+++
@@ -1,4 +0,0 @@
-class_commentary = 'ds-text-tight-m ds-font-regular ds-flex ds-px-3 ds-py-2 lg:ds-px-4 lg:ds-py-[10px] ds-items-start ds-select-none lg:ds-select-auto'
-class_score = 'ds-text-ui-typo-mid'
-class_overs = 'ds-text-tight-s ds-font-regular ds-mb-1 lg:ds-mb-0 lg:ds-mr-3 ds-block ds-text-center'
-class_for_toss = 'ds-text-tight-m ds-font-regular ds-truncate ds-text-typo-title'
